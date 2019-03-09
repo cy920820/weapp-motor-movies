f564c0a39452e40656c624b722153de9bb09a6d7
Cui-y <cuiyang673308817@gmail.com>
2019-03-09 21:06:49 +0800

    complete discuss module

--- miniprogram/pages/detail/index.js
+++ miniprogram/pages/detail/index.js
@@ -12,25 +12,38 @@ Page({
   },
 
   onLoad(options) {
-    let id = options.id
-    // let detail = wx.getStorageSync('detail')
-    // this.setData({
-    //   detail
-    // })
-    wx.showLoading({
-      title: '加载中'
+    // let id = options.id
+    let detail = wx.getStorageSync('detail')
+    this.setData({
+      detail
     })
 
-    getMovieDetail(id).then(res => {
-      wx.hideLoading()
-      let detail = res.data
-      this.setData({
-        detail
-      })
+    // wx.showLoading({
+    //   title: '加载中'
+    // })
+
+    // getMovieDetail(id).then(res => {
+    //   wx.hideLoading()
+    //   let detail = res.data
+    //   this.setData({
+    //     detail
+    //   })
+    // })
+    // .catch(err => {
+    //   wx.hideLoading()
+    //   console.error(err)
+    // })
+  },
+
+  preview() {
+    let photos = this.data.detail.photos
+    let urls = photos.map(photo => {
+      return photo.thumb
     })
-    .catch(err => {
-      wx.hideLoading()
-      console.error(err)
+
+    wx.previewImage({
+      current: urls[0], // 当前显示图片的http链接
+      urls // 需要预览的图片http链接列表
     })
   }
 })
