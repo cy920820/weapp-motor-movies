1d3a2a535a2b084f7b5dd2b89330bae373f62fca
Cui-y <cuiyang673308817@gmail.com>
2019-03-11 16:12:46 +0800

    update

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,8 +3,10 @@
 App({
   data: {
     currentCity: '北京',
-    version: '1.0.0'
+    version: '1.0.0',
+    userInfo: ''
   },
+
   onLaunch: function () {
 
     if (!wx.cloud) {
@@ -20,21 +22,5 @@ App({
         traceUser: true
       })
     }
-
-    // 查看是否授权，授权后方可获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.globalData = {
-                userInfo: res.userInfo
-              }
-            }
-          })
-        }
-      }
-    })
   }
 })
--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -4,20 +4,33 @@ let app = getApp()
 Page({
 
   data: {
-    avatarUrl: '',
-    username: '',
-    numbers: 0,
+    userinfo: {},
     email: 'cy920820@gmail.com',
     version: 'v1.0.0'
   },
 
   onLoad: function (options) {
-    // 设置用户头像
-    let userInfo = app.globalData.userInfo
-    console.log(userInfo)
+    // 查看是否授权
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGotUserInfo(e) {
+    let userinfo = e.detail.userInfo
     this.setData({
-      avatarUrl: userInfo.avatarUrl,
-      username: userInfo.nickName
+      userinfo
     })
   }
 })
